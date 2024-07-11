b73f8cc197bc928bcc882d9172cd760cc1fb4ddf
rayhanrahman25 <rayhanrahman880@gmail.com>
2024-07-11 18:05:18 +0600

    refactor: change little bit code base of password generator

--- src/App.jsx
+++ src/App.jsx
@@ -7,51 +7,46 @@ function App() {
   const [charAllowed, setCharAllowed] = useState(false)
   const [password, setPassword] = useState('')
 
-  const passwordRef = useRef(null)
+  const passRef = useRef('');
 
-  const generatePassword = useCallback(() => {
-    let pass = ""
-    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-
-    if(numberAllowed) str += "0123456789"
-    if(charAllowed) str += "!@#$%^&*()_+"
-
-    for(let i = 1; i < length; i++) {
-      const char = Math.floor(Math.random() * str.length + 1)
-      pass += str.charAt(char)
-    }
-
-    setPassword(pass)
-
-  }, [length, numberAllowed, charAllowed])
-
-  const copyPasswordToClipboard = () => {
-    window.navigator.clipboard.writeText(password)
-    passwordRef.current?.select()
-    
+  const copyToClipBoard = () => {
+      window.navigator.clipboard.writeText(password)
+      passRef.current?.select()
   }
 
+  const generatePassword = useCallback(() => {
+    let letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    let numbers = '0123456789'
+    let char = '!@#$%^&*()_+'
+    if(numberAllowed) letters += numbers
+    if(charAllowed) letters += char
+
+    let pass = ''
+    for(let i = 0; i < length; i++) {
+      const randomIndex = Math.floor( Math.random() *  letters.length )
+      pass += letters[randomIndex];
+    }
+    setPassword(pass);
+  }, [length, charAllowed, numberAllowed]);
 
   useEffect(()=>{
+    // copyToClipBoard()
     generatePassword()
-  }, [length, numberAllowed, charAllowed])
+  }, [length, numberAllowed, charAllowed, generatePassword])
 
   return (
     <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500'>
       <h1 className='text-white text-center my-3'>Password Generator</h1>
       <div className='flex shadow rounded-lg overflow-hidden mb-4'>
         <input 
+        ref={passRef}
         type="text"
-        value={password}
         className='outline-none w-full py-1 px-3'
         placeholder='Password'
+        value={password}
         readOnly
-        ref={passwordRef}
         />
-        <button
-        onClick={copyPasswordToClipboard}
-        className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'
-        >copy</button>
+        <button onClick={copyToClipBoard} className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0' >copy</button>
       </div>
       <div
       className='flex text-sm gap-x-2'
