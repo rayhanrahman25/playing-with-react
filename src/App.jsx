b5ce6f733202d734c0620744663bd609e6b7bd54
rayhanrahman25 <rayhanrahman880@gmail.com>
2024-08-12 19:26:55 +0600

    refacotr: basic routing done

--- src/App.jsx
+++ src/App.jsx
@@ -1,77 +1,13 @@
 import { useState } from 'react'
-import InputBox from './components/InputBox'
-import UseCurrencyInfo from './hooks/useCurrencyInfo'
 import './App.css'
 
 function App() {
-  const [amount, setAmount] = useState(0)
-  const [convertedAmount, setonvertedAmount] = useState(0)
-  const [fromCurrency, setFromCurrency] = useState('usd')
-  const [toCurrency, setToCurrency] = useState('bdt')
-  const [currencyDisabled, setCurrencyDisabled] = useState(true)
-
-  const currencyInfo = UseCurrencyInfo(fromCurrency);
-
-  const options = Object.keys(currencyInfo)
-
-  const convertAmount  = () => {
-    setonvertedAmount(Math.floor(amount * currencyInfo[toCurrency]))
-  }
-
-  const swap = () => {
-    setFromCurrency(toCurrency)
-    setToCurrency(fromCurrency)
-    setonvertedAmount(amount)
-    setAmount(convertedAmount)
-  }
+  const [count, setCount] = useState(0)
 
   return (
-
-    <div className='w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat'
-    style={{backgroundImage: `url(https://images.pexels.com/photos/4497591/pexels-photo-4497591.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2)`}}
-    >
-      <div className='w-full'>
-        <div className='w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30'>
-          <form onSubmit={(e) => {
-            e.preventDefault()
-          }}>
-              <div className='w-full mb-1'>
-                <InputBox 
-                    label={'Amount'}
-                    amount={amount}
-                    onAmountChange={(amount) => setAmount(amount)}
-                    currencyOptions={options}
-                    onCurrencyChange={(fromCurrency) => setFromCurrency(fromCurrency)}
-                    selectedCurrency={fromCurrency}
-                    currencyDisbled={currencyDisabled}
-                />
-              </div>
-           
-              <button
-              className='absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5'
-              onClick={swap}
-              >Swap</button>
-              <div className='w-full mb-1'>
-                <InputBox
-                label="to"
-                currencyOptions={options}
-                amount={convertedAmount}
-                onCurrencyChange={(toCurrency) => setToCurrency(toCurrency)}
-                selectedCurrency={toCurrency}
-                amountDisabled
-                />
-              </div>
-            
-              <button
-            type='submit'
-            className='w-full bg-blue-600 text-white px-4 py-3 rounded-lg' onClick={convertAmount}
-            >Convert {fromCurrency.toUpperCase()} to {toCurrency.toUpperCase()}</button>
-          </form>
-        </div>
-      </div>
-
-    </div>
-  
+    <>
+      <h1 className='bg-green-300'>{ count }</h1>
+    </>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import Home from './components/Home.jsx'
+import About from './components/About.jsx'
+import Contact from './components/Contact.jsx'
+import Layout from './Layout.jsx'
+import User from './components/User.jsx'
 import './index.css'
+import Github, { gitHubInfosLoader } from './components/Github.jsx'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+        <Route path='' element={<Home/>}></Route>
+        <Route path='about' element={<About/>}></Route>
+        <Route path='contact' element={<Contact/>}></Route>
+        <Route path='user' element={<User/>}>
+          <Route path=":userName"/>
+        </Route>
+        <Route path='github' loader={gitHubInfosLoader} element={<Github/>}></Route>
+    </Route>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
