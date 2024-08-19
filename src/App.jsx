adcab8047b36e172a898919b5bd7708f6e826c70
rayhanrahman25 <rayhanrahman880@gmail.com>
2024-08-19 18:28:05 +0600

    refactor: theme switcher feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,42 @@
 // import { useState } from 'react'
-import PricingTable  from './components/Pricing'
 
+import { useEffect, useState } from 'react'
 import './App.css'
+import ThemeBtn from './components/Button'
+import Card from './components/Card'
+import { ThemeProvider } from './contexts/ThemeProvider'
 
 function App() {
+  const [themeMode, setThemeMode] = useState('light');
+
+  const darkTheme = () => {
+    setThemeMode('dark');
+  }
+
+  const lightTheme = () => {
+    setThemeMode('light')
+  }
+
+  useEffect( () => {
+    document.querySelector('html').classList.remove('dark', "light")
+    document.querySelector('html').classList.add(themeMode)
+  }, [themeMode])
+
   return (
     <>
-      <PricingTable pricingLabel='Basic' buttonColor="red" />
-      <PricingTable pricingLabel='Medium' buttonColor="green"/>
-      <PricingTable pricingLabel='Premium' buttonColor="blue"/>
+    <ThemeProvider value={{themeMode, darkTheme, lightTheme}}>
+      <div className="flex flex-wrap min-h-screen items-center">
+          <div className="w-full">
+              <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                  <ThemeBtn />
+              </div>
+
+              <div className="w-full max-w-sm mx-auto">
+                  <Card />
+              </div>
+          </div>
+      </div>
+      </ThemeProvider>
     </>
   )
 }
